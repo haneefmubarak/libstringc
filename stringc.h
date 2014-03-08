eeca3e6642c757a4f3636e0cadc8fd167aa815db
Haneef Mubarak <haneefmubarak@users.noreply.github.com>
2014-03-08 01:30:00 -0800

    added initial header

---
+++ stringc.h
@@ -0,0 +1,11 @@
+// libstringc
+// the (missing) string library for c
+//
+// Copyright 2014 Haneef Mubarak
+// All Rights Reserved
+
+#pragma once
+
+#include <stdlib.h>
+#include <string.h>
+#include <stdint.h>
